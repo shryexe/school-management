4bd79a58ecd3676af985fbcdb5ddc62c9e7d465f
shreya <shreya.mishra.cs24@ggits.net>
2025-12-31 22:52:43 +0530

    feat(school-landing): Add App component with main layout structure

---
+++ school-landing/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import LogosRow from './components/LogosRow'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <Hero />
+      <LogosRow />
+    </div>
+  )
+}
+
+export default App
